aebf88b8dc67700b0204534b0c9e14efd7a70492
Steven <furnbirds@gmail.com>
2024-05-04 19:16:20 +0800

    added static components of table and table rows and search bar

--- src/App.js
+++ src/App.js
@@ -11,6 +11,41 @@ import Home from "./Home";
 import MedicalProfiles from "./MedicalProfiles.js"
 import './index.css';
 
+function MedProfiles() {
+  return (
+    <div>
+      <SearchBar />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th className="names">Last Name</th>
+            <th className="names">First Name</th>
+            <th className="ptGender">Gender</th>
+            <th className="ptDOB">Date of Birth</th>
+            <th className="ptOccupation">Occupation</th>
+            <th className="ptBP">Blood Pressure</th>
+            <th className="ptCondition">Condition</th>
+          </tr>
+        </thead>
+        <tbody>
+          <MedProfileRow
+            id="123"
+            surname="Johnsters"
+            firstname="Rachael"
+            gender="female"
+            dob="07/05/1999"
+            occupation="Medical Registrar"
+            systolicBP="128"
+            diastolicBP="85"
+            medicalIssue="Anaphelectic reaction to poison ivy"
+          />
+        </tbody>
+      </table>
+    </div>
+  )
+};
+
 function SearchBar() {
   return (
     <form>
@@ -19,15 +54,20 @@ function SearchBar() {
   )
 }
 
-
-function statMedicalProfiles() {
+function MedProfileRow ({ id, surname, firstname, dob, gender, occupation, systolicBP, diastolicBP, medicalIssue}) {
   return (
-    <div>
-      <SearchBar />
-    </div>
+    <tr>
+      <td className = "ptID" id = "ptID">{id}</td>
+      <td className = "names" id = "ptSurname">{surname}</td>
+      <td className = "names" id = "ptFirstname">{firstname}</td>
+      <td className = "ptGender">{gender}</td>
+      <td className = "ptDOB">{dob}</td>
+      <td className = "ptOccupation">{occupation}</td>
+      <td className = "ptBP">{systolicBP} / {diastolicBP}</td>
+      <td className = "ptCondition">{medicalIssue}</td>
+    </tr>
   )
 }
-
 
 function App() {
 
@@ -35,7 +75,7 @@ function App() {
  return (
     <div className="App">
       <NavBar />
-      <SearchBar />
+      <MedProfiles />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/MedicalProfiles" element={<MedicalProfiles />} />
@@ -45,6 +85,7 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
 
