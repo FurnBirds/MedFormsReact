8c233bfc24d5e292c9e37610ec86c7db6996fce9
Steven <furnbirds@gmail.com>
2024-05-20 00:28:35 +0800

    new profile writing to JSON, navigates to correct URL, useNavigate added

--- src/MedProfileRow.js
+++ src/MedProfileRow.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import dateFormat, { masks } from "dateformat";
 
 function MedProfileRow ({ id, surname, firstname, dob, gender, occupation, systolicBP, diastolicBP, medicalIssue}) {
   let genderShow
-
   if(gender === 1){
     genderShow = "Male"
   } else {
@@ -13,6 +13,16 @@ function MedProfileRow ({ id, surname, firstname, dob, gender, occupation, systo
       genderShow = "Other"
     }
   }
+
+  let dobShow
+
+  if(dob === null){
+    dobShow = null
+  } else{
+    dobShow = dateFormat(dob, "dd/mm/yyyy")
+  }
+  
+
   
   return (
       <tr>
@@ -20,7 +30,7 @@ function MedProfileRow ({ id, surname, firstname, dob, gender, occupation, systo
         <td className = "names" id = "ptSurname">{surname}</td>
         <td className = "names" id = "ptFirstname">{firstname}</td>
         <td className = "ptGender">{genderShow}</td>
-        <td className = "ptDOB">{dob}</td>
+        <td className = "ptDOB">{dobShow}</td>
         <td className = "ptOccupation">{occupation}</td>
         <td className = "ptBP">{systolicBP} / {diastolicBP}</td>
         <td className = "ptCondition">{medicalIssue}</td>
--- src/NewPatientProfile.js
+++ src/NewPatientProfile.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 
 function NewProfile() {
+  const [fname, setfname] = useState("");
+  const [lname, setlname] = useState("");
+  const [dob, setDOB] = useState(null);
+  const [gender, setGender] = useState(1)
   
   function handleSubmit(event) {
     event.preventDefault()
     console.log("test submit")
   }
   
+  let maleChoice = (gender === 1)
+  let femaleChoice = (gender === 0)
+  let otherChoice = (gender === 9)
   
+
+
   return(
       <div id="profileCard">
         <form onSubmit={handleSubmit}>
@@ -17,21 +28,22 @@ function NewProfile() {
           <br/><br/> */}
           <h4 id="idDisplay">ID: (New Patient Profile)</h4>
           <label for="fname" className="flabel">First name:</label><br/>
-          <input type="text" className="valueInput" id="fname" name="fname"/>
+          <input type="text" className="valueInput" id="fname" name="fname" value={fname} onChange={e => setfname(e.target.value)}/>
           <br/><br/>
           <label for="lname" className="flabel">Last name:</label><br/>
-          <input type="text" className="valueInput" id="lname" name="lname"/>
+          <input type="text" className="valueInput" id="lname" name="lname" value={lname} onChange={e => setlname(e.target.value)}/>
           <br/><br/>
           <h4>Demographics:</h4>
-          <input type="radio" className="checkInput" id="Male" name="Gender" value="1" />
+          
+          <input type="radio" className="checkInput" id="Male" name="Gender" value="1" checked={maleChoice} onChange={e=> setGender(parseInt(e.target.value))}/>
           <label for="Male" className="flabel">Male</label>
-          <input type="radio" className="checkInput" id="Female" name="Gender" value="0" />
+          <input type="radio" className="checkInput" id="Female" name="Gender" value="0" checked={femaleChoice} onChange={e=> setGender(parseInt(e.target.value))}/>
           <label for="Female" className="flabel">Female</label>
-          <input type="radio" className="checkInput" id="Other" name="Gender" value="9" />
+          <input type="radio" className="checkInput" id="Other" name="Gender" value="9" checked={otherChoice} onChange={e=> setGender(parseInt(e.target.value))}/>
           <label for="Other" className="flabel">Other</label>
           <br/><br/>
-          <label for="birthdate" className="flabel">Birth Date:</label><br/>
-          <input type="date" className="valueInput" id="birthdate" name="birthdate" />
+          <label for="birthdate" className="flabel">Birth Date:</label><br/>          
+          <input type="date" className="valueInput" id="birthdate" name="birthdate" value={dob} onChange={e => setDOB(e.target.value)}/>
           <br/><br/>
           <label for="occupation" className="occupation">Occupation:</label><br/>
           <input type="text" className="valueInput" id="occupation" name="occupation" />
